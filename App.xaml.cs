68e7f7be3ee22aff4b267bb9ff7a546f9cec26a4
echolumaque <ljhericoh15@gmail.com>
2023-11-22 03:29:46 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FullScreenIntentNotification;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FullScreenIntentNotification;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
